631c02df1f337fdb7dc0a0a86696cdc13481f9fd
Gustavo Duarte <146107422+GdDuarte@users.noreply.github.com>
2024-04-18 15:47:56 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/*Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+Lembrando que, por definição, fatorial de 0 é 1.*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        int n = sc.nextInt();
+        int fatorial = 1;
+        for (int i = 1; i <= n; i++) {
+            fatorial = fatorial * i;
+
+        }
+        System.out.println(fatorial);
+
+
+
+
+    }
+}
